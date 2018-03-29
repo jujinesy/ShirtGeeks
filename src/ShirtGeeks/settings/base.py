9bb948ca1d474e294a7ebee76c45ebaa7804f94a
jj <jujinesy@gmail.com>
2018-03-30 06:41:57 +0900

    .

--- src/ShirtGeeks/settings/base.py
+++ src/ShirtGeeks/settings/base.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+LOCAL = False
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -161,5 +163,3 @@ STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media_cdn')
-
-local = False
