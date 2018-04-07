0b5f68bd2216afa99a5a2bc7f5664ba729937ce5
jj <jujinesy@gmail.com>
2018-04-07 15:08:34 +0900

    .

--- src/conf/settings/production_test.py
+++ src/conf/settings/production_test.py
@@ -3,7 +3,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-HOSTS_NAME = 'https://sh3ltr.com'
+HOSTS_NAME = 'https://sh3ltr.org'
 ALLOWED_HOSTS = ['.sh3ltr.org']
 GOOGLE_ANALYTICS = 'UA-117029536-1'
 GOOGLE_ADSENSE = 'ca-pub-2202079831170837'
