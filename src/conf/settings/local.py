962d689b8e5460d4485192bfc16408d2a5a78f29
jj <jujinesy@gmail.com>
2018-04-08 03:44:14 +0900

    .

--- src/conf/settings/local.py
+++ src/conf/settings/local.py
@@ -6,6 +6,14 @@ ALLOWED_HOSTS = ['*']
 
 AUTO_CONFIRM = False
 HOSTS_NAME = 'http://127.0.0.1:8000'
+#HOSTS_NAME = 'http://10.211.55.7'
 
+CORS_REPLACE_HTTPS_REFERER      = False
+HOST_SCHEME                     = "http://"
+SECURE_PROXY_SSL_HEADER         = None
 SECURE_SSL_REDIRECT             = False
 SESSION_COOKIE_SECURE           = False
+CSRF_COOKIE_SECURE              = False
+SECURE_HSTS_SECONDS             = None
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
+SECURE_FRAME_DENY               = False
